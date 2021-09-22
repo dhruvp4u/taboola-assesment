5ed92912db3827ffc29ea96c26e35127cac56a33
Dhruv Ashok Padalia <padaliadhruv@gmail.com>
2021-09-22 14:00:21 -0600

    Adds Assessment class - adds two functions for the two questions - basic structure for the function (parameters, return type)

---
+++ src/Assessment.java
@@ -0,0 +1,14 @@
+public class Assessment {
+    public static Integer stringToInteger(String input){
+        return 0;
+    }
+
+    public static boolean hasInteger(String input){
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] string_to_integer_inputs = new String[] {};
+        String[] has_integer_inputs = new String[] {};
+    }
+}
